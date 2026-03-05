c47a170290ce605997dd9a6df7fbbb235a44ef59
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-05 20:18:29 +0600

    Primitive and non-primitive definition

--- primitive.js
+++ primitive.js
@@ -5,3 +5,10 @@ data = false;
 data = {price: 22};
 data = [];
 console.log(typeof data)  // ai jonno js k loslytype or weekly type ba dynamic type programming language bola hoi karon se amra ja man dibo sei tar upor base kore nijeke size kore nite pare
+
+//Primitive data type === basic data type ba simple ba normal data type
+
+//Non Primitive data type === jar moddhe primitive data type ak ba akadhik store kora hoi, ex-> array, object etc.
+
+// Primitive = Reference Type Data
+// Non-primitive = Value Type Data
