5a54f13b0469495a13506a18fe9061d30e733c60
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-05 20:11:12 +0600

    Explore let or changing value inside let

---
+++ primitive.js
@@ -0,0 +1,7 @@
+//let explore
+let data = 4;
+data = 'watermelon';
+data = false;
+data = {price: 22};
+data = [];
+console.log(typeof data)  // ai jonno js k loslytype or weekly type ba dynamic type programming language bola hoi karon se amra ja man dibo sei tar upor base kore nijeke size kore nite pare
