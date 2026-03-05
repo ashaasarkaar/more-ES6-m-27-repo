50fec8b7e9b989fa50eb0ae0c78fcdc6fa4ee351
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-05 23:48:19 +0600

    ! and git add . uses

--- truthy.js
+++ truthy.js
@@ -17,3 +17,27 @@ else{
     console.log('Falsy');
 }
 
+//output Truthy with falsy value condition
+let price = 0;
+if(price){
+    console.log('Price is Truthy'); //output asbe na
+}
+
+//output Truthy with falsy value condition with !(bang) sign ba bit(as a switch)
+let price1 = true;
+if(!price1) {
+    console.log('This is Falsy');  // vitore dhukbe na
+}
+let price2 = false;
+if(!price2) {    // ! = bit operator name   // shudhu oposite value nile
+    console.log('This is Falsy');  // vitore dhukbe 
+}
+
+//kono data type a boolean a convert korar condition
+let product = {};
+if(!!product){   //!!(double bit operator ba bang) ai ta muloto use kora hoi kono man ki type ar sei ta janar jonno
+    console.log('Truthy boolean');
+}
+
+
+
