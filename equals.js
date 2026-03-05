5fd8bcc6ff6022f053ae55aeafa4c2989740c89a
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-05 23:55:20 +0600

    ===

---
+++ equals.js
@@ -0,0 +1,7 @@
+//== vs === uses in JS
+console.log(2===2); // true
+console.log(2===3); // false
+console.log(2==='2'); // false
+
+//===
+//Compares the data type first. If data types are different it returns false, if data types are same it compares the values and returns true or false accordingly.
