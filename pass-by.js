0a9e1b2eaa889fd08677024f44952b774ed12792
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-06 08:09:45 +0600

    pass by primitive value with two phase

---
+++ pass-by.js
@@ -0,0 +1,17 @@
+// amra jodi function parameter hisebe pass kori, r value hisebe bahir theke kono akta primitive man pathai, r pathanor pore change kori, tahole, primitive man ar ager value change hobe na, jodi o ba pore change hoye jabe.
+
+function multiply(a, b){
+    a = a-5;
+    b = b-5;
+    console.log(a, b);
+    return a * b;
+}
+
+let x = 10;
+let y = 20;
+console.log('Before Calling', x, y);
+const result = multiply(x, y);
+console.log(result);
+console.log('After Calling', x, y);
+
+
