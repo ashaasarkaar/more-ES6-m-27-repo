c17db3b0caa9e20d5c4cc942764ac4a74656323e
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-06 10:58:35 +0600

    increment + decrement concepts clear

---
+++ increment.js
@@ -0,0 +1,11 @@
+let a = 5;
+a++;
+// console.log(a++); // age console log hobe pore barbe
+console.log(++a);  // age barbe pore console hobe
+console.log(a);
+
+//decrement
+let b = 10;
+b--;
+console.log(b--);
+console.log(--b);
