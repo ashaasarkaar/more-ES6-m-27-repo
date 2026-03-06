190106aab7e589a894b3498eebbd48cd8caa3ef2
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-06 10:03:11 +0600

    filter

---
+++ filter.js
@@ -0,0 +1,19 @@
+//filter
+const numbers = [2, 1, 5, 7, 8, 10];
+const evenNumbers = numbers.filter(num => num % 2 === 0); // filter ar dane condition thakbe
+// console.log(numbers);
+// console.log(evenNumbers);
+
+const friends = ['tamil', 'jamil', 'Abul', 'Akib'];
+const aFriends = friends.filter(frd => frd[0] === 'A');
+// console.log(aFriends);
+
+const students = [
+    {name: 'bablu', age: 40},
+    {name: 'Tablu', age: 31},
+    {name: 'Dhablu', age: 29},
+    {name: 'Jambu', age: 41}
+];
+const aduVaiStudents = students.filter(std => std.age > 30); //filter use korbo, jokhon kono sorto k fullfill kore, sei man ta k nite hobe
+// console.log(aduVaiStudents);
+
