dced842ce719f7943af0826a5dd5e3e3559e193b
ashaasarkaar <ashaasarkaar@gmail.com>
2026-05-27 11:43:16 +0600

    for loop + do while + wjile loop

---
+++ loop-ways-through-array.js
@@ -0,0 +1,19 @@
+// 1 way is for loop
+let array = [1, 2, 3, 4, 5, 6]
+for(let i=0; i<array.length; i++){
+    // console.log(array[i]);
+}
+
+//2nd Way is do while loop
+let i = 0;
+do{
+    // console.log(i)
+    i++;
+}while(i < 5)
+
+//3rd way is while loop
+let a = 1;
+while(a <= 5){
+    console.log(a);
+    a++;
+}
